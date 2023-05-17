83597e0c762f015dc805361e2ca82fa73ca74a34
Danil Domrachev <72508100+ddanlie@users.noreply.github.com>
2023-05-17 23:11:26 +0200

    subtraction, linear regression approx., switch

--- nmrAnalyzer.py
+++ nmrAnalyzer.py
@@ -1,9 +1,11 @@
 import tkinter as tk 
+import matplotlib as mlp
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.backend_bases import MouseEvent
 from matplotlib.widgets import SpanSelector
 from scipy.optimize import curve_fit
+from scipy.optimize import minimize
 from tkinter.font import Font
 from tkinter import filedialog
 import copy
@@ -49,24 +51,56 @@ class VerticalScrolledFrame(tk.Frame):
 
 
 class VerticalNavigationToolbar2Tk(NavigationToolbar2Tk):
-   def __init__(self, canvas, window):
-      super().__init__(canvas, window, pack_toolbar=False)
 
-   # override _Button() to re-pack the toolbar button in vertical direction
-   def _Button(self, text, image_file, toggle, command):
-      b = super()._Button(text, image_file, toggle, command)
-      b.pack(side=tk.TOP) # re-pack button in vertical direction
-      return b
+    def __init__(self, canvas, window):
+        self.toolitems = (
+            ('Home', 'Reset original view', 'home', 'home'),
+            ('Back', 'Back to previous view', 'back', 'back'),
+            ('Forward', 'Forward to next view', 'forward', 'forward'),
+            ('Pan', 'Pan axes with left mouse, zoom with right', 'move', 'pan'),
+            ('Zoom', 'Zoom to rectangle', 'zoom_to_rect', 'zoom'),
+            # TODO Get this poor thing a nice gif
+            #('Axes', 'Zoom in on region of interest (15-45)', 'subplots', 'plot_axes'),
+            ('Subplots', 'Configure subplots', 'subplots', 'configure_subplots'),
+            ('Save', 'Save the figure', 'filesave', 'save_figure'),
+            ('Scale', 'Scale', 'zoom_to_rect', 'toolbar_change_scale')
+            )
+       # NavigationToolbar2Tk.toolitems += 
+        super().__init__(canvas, window, pack_toolbar=False)
+        self.scaleButtonText = tk.StringVar(None, f"Scale: {self.canvas.figure.get_axes()[0].get_yscale()}")
+        self.custom_button = tk.Button(self, textvariable=self.scaleButtonText, command=self.toolbar_change_scale)
+        #self.custom_button.pack(side=tk.TOP)
 
-   # override _Spacer() to create vertical separator
-   def _Spacer(self):
-      s = tk.Frame(self, width=26, relief=tk.RIDGE, bg="DarkGray", padx=2)
-      s.pack(side=tk.TOP, pady=5) # pack in vertical direction
-      return s
+    
 
-   # disable showing mouse position in toolbar
-   def set_message(self, s):
-      pass
+    # override _Button() to re-pack the toolbar button in vertical direction
+    def _Button(self, text, image_file, toggle, command):
+        b = super()._Button(text, image_file, toggle, command)
+        b.pack(side=tk.TOP) # re-pack button in vertical direction
+        return b
+
+    # override _Spacer() to create vertical separator
+    def _Spacer(self):
+        s = tk.Frame(self, width=26, relief=tk.RIDGE, bg="DarkGray", padx=2)
+        s.pack(side=tk.TOP, pady=5) # pack in vertical direction
+        return s
+
+    # disable showing mouse position in toolbar
+    def set_message(self, s):
+        pass
+
+    def toolbar_change_scale(self):
+        axes = self.canvas.figure.get_axes()
+        scale = axes[0].get_yscale()
+        #print(scale)
+        if scale == 'linear':
+            for ax in axes:
+                ax.set_yscale('log')
+        else:
+            for ax in axes:
+                ax.set_yscale('linear')
+        self.scaleButtonText.set(f"Scale: {self.canvas.figure.get_axes()[0].get_yscale()}")
+        self.canvas.draw()
    
 
 class EntryScale(tk.Scale):
@@ -106,8 +140,10 @@ class NMR(tk.Tk):
         self.canvasPanel = None
         self.canvasToolPanel = None
         self.controlPanel = tk.Frame(self, width=100)
+        self.figureSettingsPanel = tk.Frame(self, borderwidth=1)
         self.bottomPanel = tk.Frame(self)
         self.editPanel = VerticalScrolledFrame(self.bottomPanel, 200, 200)
+        self.editTopPanel = tk.Frame(self.editPanel.interior)
         self.resultPanel = tk.Text(self.bottomPanel, width=23, height=10)
     
 
@@ -140,11 +176,26 @@ class NMR(tk.Tk):
         self.resetButton.grid(row=5, column=1, columnspan=2, sticky=tk.EW)
         self.chooseFileButton.grid(row=6, column=1, columnspan=2, sticky=tk.EW)
         #status panel
+
+        #figure settings
+        self.switchSelectedButton = tk.Button(self.figureSettingsPanel, text="Switch", font=self.generalFont, command=self.switchSelectedButton_pressed)
+        self.subtractButton = tk.Button(self.figureSettingsPanel, text="Subtract", font=self.generalFont, command=self.subtractButton_pressed)
+        
+        self.figureSettingsPanel.columnconfigure((0,1), uniform="Settings")
+        self.switchSelectedButton.grid(row=0, column=0, sticky=tk.EW)
+        self.subtractButton.grid(row=0, column=1, sticky=tk.EW)
+        #figure settings
         
         #edit panel
-        self.defaultButton = tk.Button(self.editPanel.interior, text="Default", font=self.generalFont, command=self.defaultButton_pressed)
-        self.defaultButton.pack(side="top", anchor="center")
+        self.defaultButton = tk.Button(self.editTopPanel, text="Default", font=self.generalFont, command=self.defaultButton_pressed)
+        self.fitButton = tk.Button(self.editTopPanel, text="Fit", font=self.generalFont, command=self.fitButton_pressed)
 
+
+        self.editTopPanel.columnconfigure((0,1), uniform="Settings")
+        self.defaultButton.grid(row=0, column=0, sticky=tk.EW)
+        self.fitButton.grid(row=0, column=1, sticky=tk.EW)
+
+        self.editTopPanel.pack(side="top", anchor="center")
         #result panel
 
         #bottom panel
@@ -153,7 +204,9 @@ class NMR(tk.Tk):
 
         #main frame
         
+
         self.bottomPanel.pack(side="bottom", fill="both", anchor="center", pady=25)
+        self.figureSettingsPanel.pack(side="bottom", anchor="center", padx=(0, 325), pady=(5,0))
         self.controlPanel.pack(side="right", anchor="center", padx=(0,25))
 
         #reset will pack canvas
@@ -164,11 +217,53 @@ class NMR(tk.Tk):
 
 
 
+    def subtractButton_pressed(self):
+        subtracted_x = self.current_figure.get_axes()[1].lines[0].get_xdata()
+        subtracted_y = self.current_figure.get_axes()[1].lines[0].get_ydata()
+        self.current_figure.clf()
+        plt.close(self.current_figure)
+        self.current_figure = self.get_curve_select_figure((subtracted_x, subtracted_y))
+        self.canvasPanel_pack(self.current_figure)
+        self.span_selector_set_axis(self.current_figure.get_axes()[0])
+
+        self.subtractButton.config(state="disabled")
+
+    def switchSelectedButton_pressed(self, trim=None, update=True):
+        x = self.current_figure.get_axes()[0].lines[0].get_xdata()
+        y = self.current_figure.get_axes()[0].lines[0].get_ydata()
+        ax = self.current_figure.get_axes()[1] 
+        ax.cla()
+        ax.set_yscale(self.current_figure.get_axes()[0].get_yscale())
+        if(not hasattr(self, "curr_switch")):
+            self.curr_switch = "subtraction"
+        if(update):
+            if(self.curr_switch == "subtraction"):
+                self.curr_switch = "curve"
+            else:
+                self.curr_switch = "subtraction"
+        if(trim != None):
+            self.curr_trim = trim
+        if(self.curr_switch  == "curve"):
+            if(hasattr(self, "curr_trim")):
+                x = x[self.curr_trim[0]:self.curr_trim[1]]
+                y = y[self.curr_trim[0]:self.curr_trim[1]]
+            y_curve = self.curve_function(x, *self.current_curve_params)
+            ax.plot(x, y, color='blue')
+            ax.plot(x, np.exp(y_curve), color='green')
+        elif(self.curr_switch  == "subtraction"):
+            y_curve = np.exp(self.curve_function(x, *self.current_curve_params))    
+            y_difference = y - y_curve
+            print(f"diff len = {len(y_difference)}")
+            ax.plot(x, y_difference, color='red')
+       
+        self.canvasPanel.draw()
+
+
     def get_curve_select_figure(self, xy):
         f = plt.figure()
         axis = f.subplots(nrows=2, ncols=1)
-        if(self.current_curve_num == 1):
-            axis[0].set_yscale("log")
+        #if(self.current_curve_num == 1):
+        axis[0].set_yscale("log")
         if(xy != None):
             axis[0].plot(xy[0], xy[1])
         return f
@@ -181,10 +276,13 @@ class NMR(tk.Tk):
 
         axes = f.subplots(nrows=2, ncols=1)
         axes[0].set_yscale("log")
-        axes[0].plot(predicted_xy[0], predicted_xy[1], "g--")
         axes[0].plot(original_xy[0], original_xy[1], "blue")
+        axes[0].plot(predicted_xy[0], predicted_xy[1], "r--")
         axes[1].plot(original_xy[0], original_xy[1]-predicted_xy[1], "red")
         return f
+
+
+
 
 
     def canvasPanel_pack(self, f):
@@ -192,7 +290,7 @@ class NMR(tk.Tk):
             self.canvasPanel.get_tk_widget().destroy()
             self.toolbar.destroy()
         self.canvasPanel = FigureCanvasTkAgg(f, master=self)
-        self.toolbar = VerticalNavigationToolbar2Tk(self.canvasPanel, self)   
+        self.toolbar = VerticalNavigationToolbar2Tk(self.canvasPanel, self)
         self.toolbar.update()
         self.toolbar.pack(side="left", anchor="center", padx=(25, 0), pady=5)
         self.canvasPanel.get_tk_widget().pack(side="left", fill="both", expand=True, padx=(0, 25), pady=(25, 0))
@@ -211,8 +309,6 @@ class NMR(tk.Tk):
         return a*x+b
 
     def curve_selected(self, xmin, xmax):
-        if(self.mode != "select"):
-            return
         x = self.current_figure.get_axes()[0].lines[0].get_xdata()
         y = self.current_figure.get_axes()[0].lines[0].get_ydata()
         indmin, indmax = np.searchsorted(x, (xmin, xmax))
@@ -221,24 +317,30 @@ class NMR(tk.Tk):
         if(indmax-indmin < 2):
             self.statuLabelVar_set("Found curve is too short")
             return
-        #print(f"{indmin}, {indmax}")
         trim_x = np.array(x[indmin:indmax])
         trim_y = np.array(y[indmin:indmax])
-        #we will aproximate ln(y)
-        trim_ylog = np.log(trim_y)
+        if(self.mode == "select"):
+            #print(f"{indmin}, {indmax}")
+
+            #we will aproximate ln(y)
+            trim_ylog = np.log(trim_y)
         
-        ax = self.current_figure.get_axes()[1] 
+            print(f"trim len = {len(trim_x)}")
+            self.current_curve_params, _ = curve_fit(self.curve_function, trim_x, trim_ylog, p0=(-1/len(trim_x), 1))
+            self.switchSelectedButton_pressed((indmin,indmax), False)
+            
+            self.saveCurveButton.config(state="normal")
+            self.previousCurveButton.config(state="disabled")
+            self.approxButton.config(state="disabled")
+            self.subtractButton.config(state="normal")
+            if(self.mode == "select"):
+                self.switchSelectedButton.config(state="normal")
+            self.statuLabelVar_set(f"Found curve.\nTime T{self.current_curve_num} = {int(-1/self.current_curve_params[0])}")
 
-        self.current_curve_params, _ = curve_fit(self.curve_function, trim_x, trim_ylog, p0=(-1/len(trim_x), 1))
-        ax.cla()
-        ax.plot(trim_x, trim_ylog, color='blue')
-        ax.plot(trim_x, self.curve_function(trim_x, *self.current_curve_params), color='green')
-        self.canvasPanel.draw_idle()
-        self.saveCurveButton.config(state="normal")
-        self.previousCurveButton.config(state="disabled")
-        self.approxButton.config(state="disabled")
-        self.statuLabelVar_set(f"Found curve {round(self.current_curve_params[0],2)}*x + {round(self.current_curve_params[1],2)}")
-
+        if(self.mode == "approx"):
+            self.trim_fit = (indmin,indmax)     
+            self.fitButton.config(state="normal")
+            self.statuLabelVar_set("Data for fitting are chosen")         
 
 
     def resetButton_pressed(self):
@@ -255,9 +357,12 @@ class NMR(tk.Tk):
         self.curveNumLabelVar.set(self.curveNumText.format(self.current_curve_num))
         self.statuLabelVar_set("reset")
         self.defaultButton.config(state="disabled")
+        self.fitButton.config(state="disabled")
+        self.subtractButton.config(state="disabled")
         self.saveCurveButton.config(state="disabled")
         self.previousCurveButton.config(state="disabled")
         self.approxButton.config(state="disabled")
+        self.switchSelectedButton.config(state="disabled")
         if(self.data == None):
             self.saveCurveButton.config(state="disabled")
 
@@ -302,13 +407,14 @@ class NMR(tk.Tk):
                 result += self.approximated_weights[i]*(np.exp(x*self.edited_curves[i][0]))
 
         if(self.mode == "approx"):
-            #plt.close(self.approx_figure)
             self.approx_figure = self.get_approximate_figure((x, result), (x, y), self.approx_figure)
             self.canvasPanel.draw()
         else:
             self.approx_figure = self.get_approximate_figure((x, result), (x, y))
             self.canvasPanel_pack(self.approx_figure)
-        #self.span_selector_set_axis(self.)
+        
+        self.span_selector_set_axis(self.approx_figure.get_axes()[0])
+        
 
         self.saveCurveButton.config(state="disabled")
         self.approxButton.config(state="disabled")
@@ -359,11 +465,11 @@ class NMR(tk.Tk):
 
     def saveCurveButton_pressed(self):
         x = self.current_figure.get_axes()[0].lines[0].get_xdata()
-        ylog_data = np.log(self.current_figure.get_axes()[0].lines[0].get_ydata())
-        y_curve = self.curve_function(x, *self.current_curve_params)
+        y_data = self.current_figure.get_axes()[0].lines[0].get_ydata()
+        y_curve = np.exp(self.curve_function(x, *self.current_curve_params))
         #subtract
-        y_difference = ylog_data - y_curve
-        trim_index = np.where(y_difference < 1e-10)[0][0]
+        y_difference = y_data - y_curve
+        trim_index = len(y_difference)#np.where(y_difference < 1e-10)[0][0]
         #print(f"trim = {trim_index}")
         y_difference = y_difference[0:trim_index]
         x = x[0:trim_index]
@@ -376,17 +482,18 @@ class NMR(tk.Tk):
         self.figure_log.append(self.current_figure)
         if(len(x) > 10):
             #set figure
+            self.current_curve_num += 1
+            self.curveNumLabelVar.set(self.curveNumText.format(self.current_curve_num))
             self.current_figure = self.get_curve_select_figure((x,y_difference))
             self.canvasPanel_pack(self.current_figure)
             self.span_selector_set_axis(self.current_figure.get_axes()[0])
-            self.current_curve_num += 1
-            self.curveNumLabelVar.set(self.curveNumText.format(self.current_curve_num))
         
         self.edited_curves = copy.deepcopy(self.curve_log)
 
         self.saveCurveButton.config(state="disabled")
         self.previousCurveButton.config(state="normal")
         self.approxButton.config(state="normal")
+        self.switchSelectedButton.config(state="disabled")
         self.statuLabelVar_set("Curve saved")
 
         
@@ -407,7 +514,7 @@ class NMR(tk.Tk):
             result += f"W{len(self.approximated_weights)-i}: {round(w,5)}\n"
         result += "\n"
         for i, params in enumerate(self.edited_curves):
-            result += f"T{len(self.edited_curves)-i}: {round((-1/params[0]),5)}\n"
+            result += f"T{len(self.edited_curves)-i}: {int((-1/params[0]))}\n"
         self.resultPanel.insert(tk.END, result)
 
     def defaultButton_pressed(self):
@@ -420,6 +527,77 @@ class NMR(tk.Tk):
             t.set(-1/self.edited_curves[i][0])
         self.statuLabelVar_set("returned to default curves")
         self.defaultButton.config(state="disabled")
+
+    def fitButton_pressed(self):
+        x = self.data[0]
+        y = self.data[1]
+        if(hasattr(self, "trim_fit")):
+            if(self.trim_fit is not None):
+                x = x[self.trim_fit[0]:self.trim_fit[1]]
+                y = y[self.trim_fit[0]:self.trim_fit[1]]
+        x = np.array(x)
+        y = np.array(y)
+        self.fit_goal = None
+        self.fit_base = None
+
+ 
+        def objective(coefs):
+            result = np.zeros_like(x)
+            for w,t in zip(coefs[len(self.edited_curves):], coefs[0:len(self.edited_curves)]):
+                result += w*np.exp(-x/(t*maxT))
+            result /= result[0]
+            print(np.mean((result - y)**2))
+            return np.mean((result - y)**2)
+
+        def w_constraint(coefs):#coefs = T3,T2,T1...w3,w2,w1...
+            ws = coefs[len(self.edited_curves):]
+            s = sum(ws)
+            res = s-1
+            if(s <= 1.0001 and s > 0.999):
+                res = 0
+            # print(coefs)
+            return res
+
+        bnds = []
+        initial_guess = []
+        maxT = 0
+        for t in self.t_vars:
+            if(t.get() > maxT):
+                maxT = t.get()
+            
+        for t in self.t_vars:
+            initial_guess.append(np.random.random())
+            bnds.append((0, 1))
+        for w in self.w_vars:
+            initial_guess.append(np.random.random())
+            bnds.append((0, 1))
+
+        self.statuLabelVar_set("Solving...")
+
+        cons = ({'type': 'eq', 'fun': w_constraint})
+        
+        solution = minimize(objective, initial_guess, bounds=bnds, constraints=cons)
+        
+        self.statuLabelVar_set("Solved")
+        
+        
+        print(solution.x)
+        self.weights_approx = False
+        self.approximated_weights = []
+        for i, t in enumerate(solution.x[0:len(self.edited_curves)]):
+            self.edited_curves[i][0] = -1/(t*maxT)
+        for i, w in enumerate(solution.x[len(self.edited_curves):]):
+            self.approximated_weights.append(w)
+
+        self.defaultButton.config(state="normal")
+        self.fitButton.config(state="disabled")
+
+        self.approxButton_pressed()
+
+ 
+
+
+
 
     def approximation_update(self, _):
         self.defaultButton.config(state="normal")
@@ -449,7 +627,10 @@ class NMR(tk.Tk):
         if(hasattr(self, "w_vars") and hasattr(self, "t_vars")):
             for i in range(len(self.w_vars)):
                 self.w_vars[i].set(self.approximated_weights[i])
-                self.t_vars[i].set(max(0, round(-1/self.edited_curves[i][0],2)))
+                self.t_vars[i].set(max(0, int(-1/self.edited_curves[i][0])))
+
+
+
 
     def add_approximation_edit(self):
         weightEditFrame = tk.Frame(self.editPanel.interior)
@@ -483,12 +664,8 @@ class NMR(tk.Tk):
         self.update()
 
     def delete_approximation_edit(self):
-        for c in self.editPanel.interior.winfo_children():
-            if(not isinstance(c, tk.Button)):
-                print(c)
+        for c in self.editPanel.interior.winfo_children()[1:]:#1st frame is a frame with buttons
                 c.destroy()
-            else:
-                print("found button")
         self.w_vars = []
         self.t_vars = []
         self.update()
@@ -516,7 +693,7 @@ class NMR(tk.Tk):
         # n = 30
         # y = np.convolve(y, np.ones(n) / n, mode='valid')
         # t = t[0:-n+1]
-        y /= y[0] 
+        y /= y[0]
 
         self.data = (t,y)
         self.resetButton_pressed()
